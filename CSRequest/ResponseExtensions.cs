60591f2a28884311a060d8e1e7f22c46d391daba
Marcelo Augusto <marceloinvest@gmail.com>
2020-10-22 22:47:52 -0300

    documentation

--- CSRequest/ResponseExtensions.cs
+++ CSRequest/ResponseExtensions.cs
@@ -10,6 +10,11 @@ namespace CSRequest
 {
     public static class ResponseExtensions
     {
+        /// <summary>
+        /// Reads the response body as string. Disposes the <see cref="HttpResponseMessage"/>.
+        /// </summary>
+        /// <param name="msg">The response.</param>
+        /// <returns>The response body as string.</returns>
         public static async Task<string> ReadStringAsync(this HttpResponseMessage msg)
         {
             try
@@ -27,6 +32,12 @@ namespace CSRequest
             }
         }
 
+        /// <summary>
+        /// Reads the json response as a <typeparamref name="T"/> object. Disposes the <see cref="HttpResponseMessage"/>.
+        /// </summary>
+        /// <typeparam name="T">The object type that the json response will be converted.</typeparam>
+        /// <param name="msg">The response.</param>
+        /// <returns>A <typeparamref name="T"/> object.</returns>
         public static async Task<T> ReadJsonAsync<T>(this HttpResponseMessage msg)
         {
             try
@@ -44,6 +55,11 @@ namespace CSRequest
             }
         }
 
+        /// <summary>
+        /// Reads the json response as a dynamic object. Disposes the <see cref="HttpResponseMessage"/>.
+        /// </summary>
+        /// <param name="msg">The response.</param>
+        /// <returns>A dynamic object.</returns>
         public static async Task<dynamic> ReadJsonAsync(this HttpResponseMessage msg)
         {
             try
@@ -61,6 +77,11 @@ namespace CSRequest
             }
         }
 
+        /// <summary>
+        /// Reads the stream content of the <see cref="HttpResponseMessage"/>. Disposes the <see cref="HttpResponseMessage"/>.
+        /// </summary>
+        /// <param name="msg">The response.</param>
+        /// <returns>A <see cref="Stream"/>.</returns>
         public static async Task<Stream> ReadStreamAsync(this HttpResponseMessage msg)
         {
             try
@@ -80,21 +101,42 @@ namespace CSRequest
             }
         }
 
+        /// <summary>
+        /// Synchronously reads the response body as string. Disposes the <see cref="HttpResponseMessage"/>.
+        /// </summary>
+        /// <param name="msg">The response.</param>
+        /// <returns>The response body as string.</returns>
         public static string ReadString(this HttpResponseMessage msg)
         {
             return msg.ReadStringAsync().Result;
         }
 
+        /// <summary>
+        /// Synchronously reads the json response as a <typeparamref name="T"/> object. Disposes the <see cref="HttpResponseMessage"/>.
+        /// </summary>
+        /// <typeparam name="T">The object type that the json response will be converted.</typeparam>
+        /// <param name="msg">The response.</param>
+        /// <returns>A <typeparamref name="T"/> object.</returns>
         public static T ReadJson<T>(this HttpResponseMessage msg)
         {
             return msg.ReadJsonAsync<T>().Result;
         }
 
+        /// <summary>
+        /// Synchronously reads the json response as a dynamic object. Disposes the <see cref="HttpResponseMessage"/>.
+        /// </summary>
+        /// <param name="msg">The response.</param>
+        /// <returns>A dynamic object.</returns>
         public static dynamic ReadJson(this HttpResponseMessage msg)
         {
             return msg.ReadJsonAsync().Result;
         }
 
+        /// <summary>
+        /// Synchronously reads the stream content of the <see cref="HttpResponseMessage"/>. Disposes the <see cref="HttpResponseMessage"/>.
+        /// </summary>
+        /// <param name="msg">The response.</param>
+        /// <returns>A <see cref="Stream"/>.</returns>
         public static Stream ReadStream(this HttpResponseMessage msg)
         {
             return msg.ReadStreamAsync().Result;
