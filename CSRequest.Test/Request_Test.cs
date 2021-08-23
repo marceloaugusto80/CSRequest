7188b11c096c19838c868a859bdbe4a051af77f7
Marcelo Augusto <marceloinvest@gmail.com>
2021-08-23 18:58:18 -0300

    resolves client base address.

---
+++ CSRequest.Test/Request_Test.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using FluentAssertions;
+using System.Net.Http;
+using CSRequest.Test.Helpers;
+
+namespace CSRequest
+{
+    public class Request_Test
+    {
+        [Fact]
+        public async Task Initialization_inject_client()
+        {
+            const string url = @"http://postman-echo.com";
+            using var client = new HttpClient()
+            {
+                BaseAddress = new Uri(url, UriKind.Absolute)
+            };
+
+            using var response = await new Request(client).WithSegments("get").GetAsync();
+            var echo = await response.ReadJsonAsync<EchoResponse>();
+            
+            echo.Url.Should().Be(url + "/get");
+        }
+
+        [Fact]
+        public async Task Initialization_inject_url_and_client()
+        {
+            const string url = @"http://postman-echo.com";
+            using var client = new HttpClient();
+
+            using var response = await new Request(url, client).WithSegments("get").GetAsync();
+            var echo = await response.ReadJsonAsync<EchoResponse>();
+
+            echo.Url.Should().Be(url + "/get");
+        }
+
+        [Fact]
+        public async Task Initialization_inject_url()
+        {
+            const string url = @"http://postman-echo.com";
+
+            using var response = await new Request(url).WithSegments("get").GetAsync();
+            var echo = await response.ReadJsonAsync<EchoResponse>();
+
+            echo.Url.Should().Be(url + "/get");
+        }
+
+        [Fact]
+        public async Task Initialization_inject_nothing()
+        {
+            Func<Task> action = ()=> new Request().WithSegments("get").GetAsync();
+            
+            await action.Should().ThrowAsync<Exception>().WithMessage("Could not resolve request url*");
+        }
+
+        [Fact]
+        public async Task Initialization_null_client()
+        {
+            HttpClient client = null;
+            Func<Task> action = () => new Request(client).WithSegments("get").GetAsync();
+
+            await action.Should().ThrowAsync<Exception>().WithMessage("You must define a*");
+        }
+    }
+}
